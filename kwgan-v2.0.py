76824c7a4febc5091744d995113f4b536113f5ec
Fede Camara Halac <camarafede@gmail.com>
2020-05-06 11:27:11 -0400

    added gpu growth flag

--- kwgan-v2.0.py
+++ kwgan-v2.0.py
@@ -169,24 +169,23 @@ def get_discriminator():
   model.summary()
   return model
 
-#  I would say no longer necessary!
-# #------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # # set allow growth flag 
 # # issue here https://github.com/tensorflow/tensorflow/issues/36025
 # # and https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth
 # #------------------------------------------------------------------------------
 
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# if gpus:
-#   try:
-#     # Currently, memory growth needs to be the same across GPUs
-#     for gpu in gpus:
-#       tf.config.experimental.set_memory_growth(gpu, True)
-#     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-#   except RuntimeError as e:
-#     # Memory growth must be set before GPUs have been initialized
-#     print(e)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
 #------------------------------------------------------------------------------
 # check gpu
