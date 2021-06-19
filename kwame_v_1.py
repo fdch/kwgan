c768767e59252d9f46da31cb657bc371e6301d99
fdch <fch226@nyu.edu>
2021-06-19 15:22:25 -0400

    fix phase shuffle class to accept name

--- kwame_v_1.py
+++ kwame_v_1.py
@@ -28,10 +28,9 @@ def get_waveform(file_path):
 class PhaseShuffle(tf.keras.layers.Layer):
 
   def __init__(self, rad=2, pad_type='reflect', name='PhaseShuffle'):
-    super(PhaseShuffle, self).__init__()
+    super(PhaseShuffle, self).__init__(name=name)
     self.rad = rad
     self.pad_type = pad_type
-    self.name = name
 
   def build(self, shape):
     ph_init = tf.random_uniform_initializer(minval=-rad, maxval=rad+1)
