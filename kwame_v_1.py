0be1ba398cc60ba8b72493e57c5fe90f98b769b6
fdch <fch226@nyu.edu>
2021-06-19 17:45:44 -0400

    fix loss functionswq

--- kwame_v_1.py
+++ kwame_v_1.py
@@ -60,7 +60,7 @@ def D_lossFun(x, z):
 
     gen = G(z)
     x_hat = epsilon * x + (1 - epsilon) * tf.squeeze(gen) 
-    d_hat = D(tf.expand_dims(x_hat, axis=0))
+    d_hat = D(tf.expand_dims(x_hat, axis=-1))
 
     ddx = tf.gradients(d_hat, x_hat)[0]
     ddx = tf.sqrt(tf.reduce_sum(tf.square(ddx), axis=1))
